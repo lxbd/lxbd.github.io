6bc5bb0a4fb57da9a7531da623ac853bdbc9d8e0
lx8d <lx8d@163.com>
2024-04-25 06:17:32 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://dev-lx8d.pantheonsite.io/";//your website url
+  let target = "https://lx8d.rr.nu/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
